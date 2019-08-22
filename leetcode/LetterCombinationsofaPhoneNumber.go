0f4b4242e914c4cdfca1b5b09ccf45b74090a3b8
crystalcd <1627566612@qq.com>
2019-08-22 21:28:34 +0800

    bfs

--- leetcode/LetterCombinationsofaPhoneNumber.go
+++ leetcode/LetterCombinationsofaPhoneNumber.go
@@ -11,7 +11,12 @@ Example:
 
 Input: "23"
 Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+
+dfs 深度优先遍历
+以2为准有三个方向连接3分别为abc，选定其中的a,3有三个方向def,选择其中的d,到底了,向上回溯,def都遍历完成在向上回溯.
+
 */
+// 深度优先
 func LetterCombinations(digits string) []string {
 	rs := make([]string, 0)
 	if len(digits) == 0 {
@@ -26,7 +31,6 @@ func LetterCombinations(digits string) []string {
 	strMap["7"] = "pqrs"
 	strMap["8"] = "tuv"
 	strMap["9"] = "wxyz"
-
 	dfs(digits, strMap, 0, "", &rs)
 	return rs
 }
@@ -39,4 +43,32 @@ func dfs(digits string, dict map[string]string, level int, curStr string, rs *[]
 	for _, c := range dict[string(digits[level])] {
 		dfs(digits, dict, level+1, curStr+string(c), rs)
 	}
+}
+
+// 广度优先
+func LetterCombinations1(digits string) []string {
+	rs := make([]string, 0)
+	if len(digits) == 0 {
+		return rs
+	}
+	strMap := make(map[string]string)
+	strMap["2"] = "abc"
+	strMap["3"] = "def"
+	strMap["4"] = "ghi"
+	strMap["5"] = "jkl"
+	strMap["6"] = "mno"
+	strMap["7"] = "pqrs"
+	strMap["8"] = "tuv"
+	strMap["9"] = "wxyz"
+	rs = append(rs, "")
+	for _, c := range digits {
+		tmp := make([]string, 0)
+		for _, r := range rs {
+			for _, t := range strMap[string(c)] {
+				tmp = append(tmp, r+string(t))
+			}
+		}
+		rs = tmp
+	}
+	return rs
 }

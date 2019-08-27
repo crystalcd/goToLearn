1c8a10ee7733e44c5483d84fe885f5ea35edfd1a
crystalcd <1627566612@qq.com>
2019-08-27 17:42:49 +0800

    合并两个有序链表

---
+++ leetcode/MergeTwoSortedLists.go
@@ -0,0 +1,47 @@
+package leetcode
+
+import "fmt"
+
+/**
+Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+
+Example:
+
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
+*/
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func MergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	tmp := new(ListNode)
+	rs := tmp
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+	for l1 != nil || l2 != nil {
+		if l1 == nil {
+			tmp.Next = l2
+			l2 = l2.Next
+		} else if l2 == nil {
+			tmp.Next = l1
+			l1 = l1.Next
+		} else if l1.Val < l2.Val {
+			tmp.Next = l1
+			l1 = l1.Next
+		} else {
+			tmp.Next = l2
+			l2 = l2.Next
+		}
+		tmp = tmp.Next
+		fmt.Println(tmp.Val)
+	}
+	return rs.Next
+}

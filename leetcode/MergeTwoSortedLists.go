d1c99b35785ca55193e4e82937edf1672cc9b4ab
crystalcd <1627566612@qq.com>
2019-08-27 17:48:35 +0800

    合并两个有序链表

--- leetcode/MergeTwoSortedLists.go
+++ leetcode/MergeTwoSortedLists.go
@@ -26,6 +26,35 @@ func MergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 	if l2 == nil {
 		return l1
 	}
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			tmp.Next = l1
+			l1 = l1.Next
+		} else {
+			tmp.Next = l2
+			l2 = l2.Next
+		}
+		tmp = tmp.Next
+		fmt.Println(tmp.Val)
+	}
+	if l1 != nil {
+		tmp.Next = l1
+	}
+	if l2 != nil {
+		tmp.Next = l2
+	}
+	return rs.Next
+}
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	tmp := new(ListNode)
+	rs := tmp
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
 	for l1 != nil || l2 != nil {
 		if l1 == nil {
 			tmp.Next = l2

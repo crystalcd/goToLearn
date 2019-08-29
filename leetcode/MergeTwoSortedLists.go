6ab402725882d49df812aad1d3f9f99c3900ca73
crystalcd <1627566612@qq.com>
2019-08-29 21:49:08 +0800

    合并两个排序链表添加一个更简便方法

--- leetcode/MergeTwoSortedLists.go
+++ leetcode/MergeTwoSortedLists.go
@@ -17,6 +17,21 @@ Output: 1->1->2->3->4->4
  *     Next *ListNode
  * }
  */
+func MergeTwoLists1(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+	if l1.Val < l2.Val {
+		l1.Next = MergeTwoLists1(l1.Next, l2)
+		return l1
+	} else {
+		l2.Next = MergeTwoLists1(l1, l2.Next)
+		return l2
+	}
+}
 func MergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 	tmp := new(ListNode)
 	rs := tmp

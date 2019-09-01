6ddf2b04874b6b0091706bc7598454fa21d83f53
crystalcd <1627566612@qq.com>
2019-09-01 09:00:20 +0800

    O(n)求解最大子数组

--- leetcode/MaximumSubarray.go
+++ leetcode/MaximumSubarray.go
@@ -1,5 +1,7 @@
 package leetcode
 
+import "math"
+
 /**
 Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
 
@@ -10,5 +12,17 @@ Output: 6
 Explanation: [4,-1,2,1] has the largest sum = 6.
 */
 func maxSubArray(nums []int) int {
+	rs, curSum := math.MinInt32, 0
+	for _, num := range nums {
+		curSum = max(curSum+num, num)
+		rs = max(rs, curSum)
+	}
+	return rs
+}
 
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }

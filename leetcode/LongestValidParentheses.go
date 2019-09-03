959b802d51b80252b8462a784eedde743ec174d8
crystalcd <1627566612@qq.com>
2019-09-03 21:01:26 +0800

    最长合法括号

---
+++ leetcode/LongestValidParentheses.go
@@ -0,0 +1,21 @@
+package leetcode
+
+import "strings"
+
+/**
+Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
+
+Example 1:
+
+Input: "(()"
+Output: 2
+Explanation: The longest valid parentheses substring is "()"
+Example 2:
+
+Input: ")()())"
+Output: 4
+Explanation: The longest valid parentheses substring is "()()"
+*/
+func longestValidParentheses(s string) int {
+
+}

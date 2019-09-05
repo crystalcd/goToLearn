cb11f5fda79ad17a759446a3c450ce74fc7b27b3
crystalcd <1627566612@qq.com>
2019-09-05 20:54:31 +0800

    二分搜索

--- leetcode/SearchinRotatedSortedArray.go
+++ leetcode/SearchinRotatedSortedArray.go
@@ -19,7 +19,50 @@ Example 2:
 
 Input: nums = [4,5,6,7,0,1,2], target = 3
 Output: -1
-*/
-func search(nums []int, target int) int {
+0　　1　　2　　 4　　5　　6　　7
 
+7　　0　　1　　 2　　4　　5　　6
+
+6　　7　　0　　 1　　2　　4　　5
+
+5　　6　　7　　 0　　1　　2　　4
+
+4　　5　　6　　7　　0　　1　　2
+
+2　　4　　5　　6　　7　　0　　1
+
+1　　2　　4　　5　　6　　7　　0
+
+二分搜索法的关键在于获得了中间数后，
+判断下面要搜索左半段还是右半段，
+我们观察上面红色的数字都是升序的，
+由此我们可以观察出规律，如果中间的数小于最右边的数，
+则右半段是有序的，若中间数大于最右边数，则
+左半段是有序的，我们只要在有序的半段里用首尾
+两个数组来判断目标值是否在这一区域内，这样就可以确定保留哪半边了，
+代码如下：
+*/
+func Search(nums []int, target int) int {
+	l, r := 0, len(nums)-1
+	for l <= r {
+		mid := (l + r) / 2
+		if nums[mid] == target {
+			return mid
+		}
+		if nums[mid] < nums[r] {
+			if target <= nums[r] && nums[mid] < target {
+				l = mid + 1
+			} else {
+				r = mid - 1
+			}
+		} else {
+			if nums[l] <= target && nums[mid] > target {
+				r = mid - 1
+			} else {
+				l = mid + 1
+			}
+		}
+
+	}
+	return -1
 }
